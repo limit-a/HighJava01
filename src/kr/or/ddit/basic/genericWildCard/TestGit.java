7fab57adc444b8b08d73d1b5c635a119e5c6ba4f
ohjihyung <superstaa@naver.com>
2022-08-22 19:35:10 +0900

    gittest.java modified

--- src/kr/or/ddit/basic/genericWildCard/TestGit.java
+++ src/kr/or/ddit/basic/genericWildCard/TestGit.java
@@ -1,10 +1,11 @@
 package kr.or.ddit.basic.genericWildCard;
 
+import java.util.ArrayList;
+
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ArrayList<String> a = new ArrayList<>();
 	}
 
 }
