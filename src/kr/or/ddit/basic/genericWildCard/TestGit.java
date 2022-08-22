27c57b51cdec67a3ca131537c0bd7c71a70ed323
limit-a <wjswogks152@gmail.com>
2022-08-22 19:38:43 +0900

    테스트깃 sysout

--- src/kr/or/ddit/basic/genericWildCard/TestGit.java
+++ src/kr/or/ddit/basic/genericWildCard/TestGit.java
@@ -3,7 +3,7 @@ package kr.or.ddit.basic.genericWildCard;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("hello");
 
 	}
 
